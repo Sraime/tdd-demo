9da086184957df96b5479ab84d6d7b0fda768a4c
r.collas <robin.collas@hotmail.fr>
2019-03-18 18:50:29 +0100

    US1 E2E1 Red (gherkin -> steps)

--- test/e2e/steps/steps.js
+++ test/e2e/steps/steps.js
@@ -4,3 +4,13 @@ const I = actor();
 Given('Je suis sur la page d\'accueil', () => {
   I.amOnPage('http://localhost:3000');
 });
+
+Then('La fonctionnalité d\'enregitrement est présente', () => {
+  I.seeElement('#form-enregistrement');
+});
+
+Then('La fonctionnalité d\'enregitrement est dans son état initial', () => {
+  I.seeInField('nom', '');
+  I.seeInField('prenom', '');
+  I.seeInField('tel', '');
+});
