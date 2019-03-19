1d4d3325999d76a795e7a159a3b2713ef4d43569
Robin <robin.collas@hotmail.fr>
2019-03-19 14:29:57 +0100

    US1 E2E2 TU2 Green (main.js)

--- statics/main.js
+++ statics/main.js
@@ -1,5 +1,5 @@
 this.onFieldChange = function onFieldChange() {
-  document.getElementById('input-nom');
+  if (document.getElementById('input-nom').value === '') return;
   document.getElementById('input-prenom');
   document.getElementById('input-tel');
   document.getElementById('btn-enregistrer').disabled = false;
