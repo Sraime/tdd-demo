ee33aca436f172f8aa56209969374be49a750ee1
Robin <robin.collas@hotmail.fr>
2019-03-19 14:48:31 +0100

    US1 E2E3 TU4 Refactoring (main.js)

--- statics/main.js
+++ statics/main.js
@@ -1,6 +1,7 @@
 this.onFieldChange = function onFieldChange() {
-  if (document.getElementById('input-nom').value === ''
-    || document.getElementById('input-prenom').value === ''
-    || document.getElementById('input-tel').value === '') return;
-  document.getElementById('btn-enregistrer').disabled = false;
+  if (document.getElementById('input-nom').value !== ''
+          && document.getElementById('input-prenom').value !== ''
+          && document.getElementById('input-tel').value !== '') {
+    document.getElementById('btn-enregistrer').disabled = false;
+  }
 };
