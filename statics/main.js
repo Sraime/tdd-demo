91ab273cb9d482130f49d64be61759ae3c7d2506
Robin <robin.collas@hotmail.fr>
2019-03-19 14:39:52 +0100

    US1 E2E2 TU3 Refactoring (main.js)

--- statics/main.js
+++ statics/main.js
@@ -1,6 +1,6 @@
 this.onFieldChange = function onFieldChange() {
-  if (document.getElementById('input-nom').value === '') return;
-  if (document.getElementById('input-prenom').value === '') return;
+  if (document.getElementById('input-nom').value === ''
+    || document.getElementById('input-prenom').value === '') return;
   document.getElementById('input-tel');
   document.getElementById('btn-enregistrer').disabled = false;
 };
