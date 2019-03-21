34f1933e4876d90a88b095f5812c63aa1b028429
r.collas <robin.collas@hotmail.fr>
2019-03-21 15:54:55 +0100

    US1 E2E3 TU10 (carnet-service.validateContact)

--- app/services/carnet-service.js
+++ app/services/carnet-service.js
@@ -3,16 +3,21 @@ const CarnetService = {
   list: [],
 
   save(object) {
-    const errors = this.validateContact(object); 
-    if(errors) return errors;
+    const errors = this.validateContact(object);
+    if (errors) return errors;
     this.list.push(object);
   },
 
   validateContact(object) {
-    if(!object.tel)
-      return { missing: [ 'tel' ] };
-    return null;
-  }
+    let missing;
+    const required = ['nom', 'tel'];
+    required.forEach((prop) => {
+      if (!object[prop]) {
+        missing = [prop];
+      }
+    });
+    return missing ? { missing } : null;
+  },
 
 };
 
