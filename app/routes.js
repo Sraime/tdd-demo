0abd7ba7d38df663a2198969e166e4481305c85f
r.collas <robin.collas@hotmail.fr>
2019-03-21 14:36:28 +0100

    US1 E2E4 Green

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const carnetController = require('./controllers/carnet-controller');
 
 router.get('/', carnetController.getForm);
+router.post('/', carnetController.addContact);
 
 
 module.exports = router;
