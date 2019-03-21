6fdd5d5e98a951c844e9e3ae7be51cc859b16eb1
r.collas <robin.collas@hotmail.fr>
2019-03-21 13:53:30 +0100

    US1 E2E4 TU1 (carnet-controller.addContact)

--- app/controllers/carnet-controller.js
+++ app/controllers/carnet-controller.js
@@ -1,7 +1,14 @@
+const carnetService = require('../services/carnet-service');
+
 const CarnetController = {
 
   getForm(req, res) {
     res.render('index');
+  },
+
+  addContact(req, res) {
+    carnetService.save({ nom: "CANARY", prenom: "Alice", tel: "0102030405" });
+    res.render('index', { nom: "CANARY", prenom: "Alice" });
   },
 };
 
--- test/unit/controllers/carnet-controller-test.js
+++ test/unit/controllers/carnet-controller-test.js
@@ -7,6 +7,7 @@ const { expect } = chai;
 chai.use(chaiSinon);
 
 const carnetController = require('../../../app/controllers/carnet-controller');
+const carnetService = require('../../../app/services/carnet-service');
 
 describe('carnet controller', () => {
   describe('getForm', () => {
@@ -20,6 +21,35 @@ describe('carnet controller', () => {
       carnetController.getForm({}, response);
 
       expect(response.render).to.have.been.called;
+    });
+  });
+
+  describe('addContact', () => {
+    let response;
+    let saveFct;
+    beforeEach(() => {
+      response = {
+        render: sinon.fake(),
+      };
+
+      saveFct = sinon.stub(carnetService, 'save');
+    });
+    afterEach(() => {
+      saveFct.restore();
+    });
+    it('should save Alice CANARY', () => {
+      const request = {
+        body: {
+          nom: 'CANARY',
+          prenom: 'Alice',
+          tel: '0102030405',
+        },
+      };
+
+      carnetController.addContact(request, response);
+
+      expect(response.render).to.have.been.calledWith('index', { nom: 'CANARY', prenom: 'Alice' });
+      expect(saveFct).to.have.been.calledWith(request.body);
     });
   });
 });
