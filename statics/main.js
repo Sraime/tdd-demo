f397cd3b71f6327c0dfeb06221c9ceb83572a1a3
Unknown <robin.collas@hotmail.fr>
2019-05-10 12:00:07 +0200

    US1 E2E2 TU1 Red (main.js)

---
+++ statics/main.js
@@ -0,0 +1,1 @@
+this.onFieldChange = function onFieldChange() {};
---
+++ test/unit/statics/main-test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiSinon = require('chai-sinon');
+
+const { expect } = chai;
+chai.use(chaiSinon);
+
+const main = require('../../../statics/main');
+
+describe('main js static file', () => {
+  describe('onFieldChange', () => {
+    let objBtn;
+    beforeEach(() => {
+      global.document = {
+        getElementById: sinon.stub(),
+      };
+
+      objBtn = { disabled: true };
+      global.document.getElementById.withArgs('btn-enregistrer').returns(objBtn);
+    });
+
+    it('sould enable button button when all field are complited', () => {
+      global.document.getElementById.withArgs('input-nom').returns({ value: 'NNNN' });
+      global.document.getElementById.withArgs('input-prenom').returns({ value: 'PPPP' });
+      global.document.getElementById.withArgs('input-tel').returns({ value: '000000' });
+
+      main.onFieldChange();
+
+      expect(global.document.getElementById).to.have.been.calledWith('input-nom');
+      expect(global.document.getElementById).to.have.been.calledWith('input-prenom');
+      expect(global.document.getElementById).to.have.been.calledWith('input-tel');
+      expect(global.document.getElementById).to.have.been.calledWith('btn-enregistrer');
+      expect(objBtn.disabled).to.equal(false);
+    });
+  });
+});
