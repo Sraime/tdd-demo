1e43a5a3aee369714d2cfe404e05482e2c657192
Unknown <robin.collas@hotmail.fr>
2019-05-10 12:21:31 +0200

    US1 E2E3 Red

--- test/e2e/steps/steps.js
+++ test/e2e/steps/steps.js
@@ -16,3 +16,17 @@ Then('Je ne peux pas enregistrer le contact', () => {
 Then('Je peux enregistrer le contact', () => {
   I.seeElement('#form-enregistrement .btn-save:not(:disabled)');
 });
+
+When('Je valide l\'enregistrement du contact', () => {
+  I.click('#form-enregistrement .btn-save');
+});
+
+Then('Le message {string} s\'affiche', (message) => {
+  I.see(message);
+});
+
+Then('La fonctionnalité d\'enregitrement est dans son état initial', () => {
+  I.seeInField('nom', '');
+  I.seeInField('prenom', '');
+  I.seeInField('tel', '');
+});
