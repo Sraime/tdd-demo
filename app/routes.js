2e7b638224720c1f620dc4bdd004bd11cd09647a
Unknown <robin.collas@hotmail.fr>
2019-05-10 11:33:54 +0200

    US1 E2E1 Green

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const carnetController = require('./controllers/carnet-controller');
+
+router.get('/', carnetController.getForm);
+
 
 module.exports = router;
